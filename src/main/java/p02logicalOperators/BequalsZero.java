ceb00e4bed6a8dc5d706e48b7ed01cf179091c3c
SamKar <letmedevelopsam@gmail.com>
2021-06-19 23:30:51 +0300

    Logical Operators

---
+++ src/main/java/p02logicalOperators/BequalsZero.java
@@ -0,0 +1,15 @@
+package p02logicalOperators;
+
+public class BequalsZero {
+    public static void main(String[] args) {
+
+        int b = 2;
+        boolean res = ++b == 2 || --b == 2 && --b ==2;
+
+        // 3  = 2  or 2  = 2 and 1 = 2
+        //	F    or T   and F
+        // T or F
+        // F
+        System.out.println(res);
+    }
+}
---
+++ src/main/java/p02logicalOperators/xEqualsTrue.java
@@ -0,0 +1,18 @@
+package p02logicalOperators;
+
+public class xEqualsTrue {
+    public static void main(String[] args) {
+
+        boolean x = true, z = true;
+        int y = 20;
+
+        x = (y!=10) || (z = false);
+
+        //  T or F
+        //   T
+
+
+        System.out.println(x);
+
+    }
+}
