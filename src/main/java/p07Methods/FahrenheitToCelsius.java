08d04b675b997c8d4a3150887f9a59c4d563b277
SamKar <letmedevelopsam@gmail.com>
2021-07-02 23:53:45 +0300

    Methods

---
+++ src/main/java/p07Methods/FahrenheitToCelsius.java
@@ -0,0 +1,22 @@
+package p07Methods;
+
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+
+		/*
+		Write a function that accepts Fahrenheit and displays the Celcius in the console.
+		Sample Output:
+			calculateCelcius(32) -- > 0
+			calculateCelcius(50) --> 10
+		*/
+
+        fahrenheitToCelsius(32);
+    }
+    public static void fahrenheitToCelsius(double fahrenheit) {
+
+        double celcius = (fahrenheit-32)/1.8;
+
+        System.out.println(fahrenheit  + " fahrenheit  is " + celcius + " degrees celcius.") ;
+    }
+
+}
