ad0e7d077733a72a50d57687ab180190f1bb5fc1
SamKar <letmedevelopsam@gmail.com>
2021-07-03 21:19:49 +0300

    Methods

--- src/main/java/p07Methods/PassMultipleArgs.java
+++ src/main/java/p07Methods/PassMultipleArgs.java
@@ -2,12 +2,14 @@ package p07Methods;
 
 public class PassMultipleArgs {
     public static void main(String[] args) {
+
         showSum(5, 10);
         showSum(12, 13);    // dont write data type here
 
     }
 
     public static void showSum(int a, int b) {
+
         System.out.println(a+b);
     }
 }
